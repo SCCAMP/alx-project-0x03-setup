867042f3a650b41c747d34560deda33dd7a8223a
SCCAMP <msprexy1@gmail.com>
2025-11-22 21:47:51 +0100

    completed all tasks

--- alx-project-0x03/components/common/Button.tsx
+++ alx-project-0x03/components/common/Button.tsx
@@ -1,10 +1,5 @@
 
-interface ButtonProps {
-  buttonLabel: string
-  buttonSize?: string
-  buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
-  action?: () => void
-}
+import { ButtonProps } from '@/interface';
 
 
 const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: ButtonProps) => {
@@ -14,7 +9,8 @@ const Button = ({ buttonLabel, buttonSize, buttonBackgroundColor, action }: Butt
     blue: 'bg-blue-500',
     orange: 'bg-orange-500',
     green: 'bg-green-500',
-  }[buttonBackgroundColor] : 'bg-slate-500'
+  }
+  [buttonBackgroundColor] : 'bg-slate-500'
 
 
   return (
